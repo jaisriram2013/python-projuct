32e23c25da00ebc8f47bb83fee6e70bec0c869df
jaisriram2013 <166018232+jaisriram2013@users.noreply.github.com>
2024-04-04 17:20:41 +0530

    Add files via upload

---
+++ projuct 1.py
@@ -0,0 +1,12 @@
+# traffic Light Simulator
+
+trafficlight=input("Enter Singal - Red or Orange or Green : ")
+trafficlight=trafficlight.lower()
+if trafficlight=="red":
+    print("STOP")
+
+elif trafficlight=="orange":
+    print("GET READY")
+
+else:
+    print("GO")
